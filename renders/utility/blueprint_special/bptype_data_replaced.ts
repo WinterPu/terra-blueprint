3b3e71a42f5e2830fcbaa962d61f273f1da5e81c
WinterPu <striveant@outllook.com>
2025-04-28 15:55:09 +0800

    Update bptype_data_replaced.ts

--- renders/utility/blueprint_special/bptype_data_replaced.ts
+++ renders/utility/blueprint_special/bptype_data_replaced.ts
@@ -73,4 +73,39 @@ int UAgoraBPuMediaPlayer::SetPlayerOptionInInt(const FString & key, int value)
           }
                 `,
   },
+
+  'agora::rtc::IMusicPlayer.openWithSongCode': {
+    doReplceDecl: false,
+    decl: ``,
+    doReplceImpl: true,
+    impl: `
+  int UAgoraBPuMusicPlayer::OpenWithSongCode(int64 songCode, int64 startPos)
+  {
+      // Need to be optimized
+      int FinalReturnResult = AGORA_UE_ERR_CODE(ERROR_NULLPTR);
+
+
+
+
+      // Convert UEBP to CppType
+      int64_t Raw_songCode = songCode;
+      int64_t Raw_startPos = startPos;
+
+      // Call Native Method
+      
+      auto ret = _NativePtr->open(Raw_songCode, Raw_startPos);
+
+      // Free Data if neeeded
+      
+
+      int ReturnVal = ret;
+      FinalReturnResult =  ReturnVal;
+
+
+      // Need to be optimized
+      return FinalReturnResult;
+
+  }
+                `,
+  },
 };
